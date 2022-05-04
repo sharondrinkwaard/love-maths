290824b652190209327fc513fe3ff8cb8d14e202
sharondrinkwaard <sharondrinkwaard@gmail.com>
2022-05-04 21:24:40 +0000

    Add code for subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,6 +108,7 @@ function displayAdditionQuestion(operand1, operand2) {
 function displaySubstractQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
     document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById("operator").textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
